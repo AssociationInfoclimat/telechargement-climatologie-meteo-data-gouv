3dfc21ab5c1c4957fa2f936b29f1de98400aac10
jlecordier <jeanlecordier@hotmail.fr>
2024-09-17 14:52:12 +0200

    archives: departements: add getDepartements

---
+++ src/archives/departements/Departement.ts
@@ -0,0 +1,29 @@
+export class InvalidDepartementError extends Error {
+    constructor(numero: number) {
+        super(`Invalid departement: '${numero}'. Must be between 1 and 95, 971 and 975, or 984 and 988.`);
+    }
+}
+
+export class Departement {
+    private readonly numero: string;
+
+    private constructor(departement: string) {
+        this.numero = departement;
+    }
+
+    static of(numero: number | string): Departement {
+        numero = parseInt(numero.toString(), 10);
+        if (!((1 <= numero && numero <= 95) || (971 <= numero && numero <= 975) || (984 <= numero && numero <= 988))) {
+            throw new InvalidDepartementError(numero);
+        }
+        return new Departement(numero.toString().padStart(2, '0'));
+    }
+
+    value(): string {
+        return this.numero;
+    }
+
+    toString(): string {
+        return this.numero;
+    }
+}
