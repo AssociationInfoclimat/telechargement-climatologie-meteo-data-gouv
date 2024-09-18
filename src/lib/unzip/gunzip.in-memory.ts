d19ded739cf1621d2ba48968e46514c08170bcec
jlecordier <jeanlecordier@hotmail.fr>
2024-09-18 16:28:16 +0200

    lib: unzip: in memory: add file system

--- src/lib/unzip/gunzip.in-memory.ts
+++ src/lib/unzip/gunzip.in-memory.ts
@@ -1,9 +1,16 @@
+import { InMemoryFileSystem } from '@/lib/fs/fileExists.in-memory.js';
 import { Unzipper } from '@/lib/unzip/Unzipper.js';
 
 export class UnzipperSpy {
     calls: { gzpath: string; outpath: string }[] = [];
+    private readonly fs: InMemoryFileSystem;
+
+    constructor({ fs = new InMemoryFileSystem() }: { fs?: InMemoryFileSystem } = {}) {
+        this.fs = fs;
+    }
 
     call(gzpath: string, outpath: string): void {
+        this.fs.add(outpath);
         this.calls.push({ gzpath, outpath });
     }
 }
