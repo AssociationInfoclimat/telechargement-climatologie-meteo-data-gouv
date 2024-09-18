ccf05262a2e5a5e2e4e220b14efd25455c520545
jlecordier <jeanlecordier@hotmail.fr>
2024-09-18 11:34:17 +0200

    fix: tests: lib: download: download readme to downloads dir

--- tests/lib/download/downloadUsingCurl.integration.spec.ts
+++ tests/lib/download/downloadUsingCurl.integration.spec.ts
@@ -1,22 +1,17 @@
 import { downloadUsingCurl } from '@/lib/download/downloadUsingCurl.js';
 import { existsSync } from 'node:fs';
-import { unlink } from 'node:fs/promises';
+import { rm } from 'node:fs/promises';
 import { beforeEach, describe, expect, it } from 'vitest';
 
 describe('downloadUsingCurl', () => {
     beforeEach(async () => {
-        try {
-            await unlink(`${import.meta.dirname}/tsconfig.json`);
-        } catch {
-            //
-        }
+        await rm(`${import.meta.dirname}/downloads`, { recursive: true, force: true });
     });
     it('should download the file to the directory', async () => {
         await downloadUsingCurl(
-            'https://raw.githubusercontent.com/AssociationInfoclimat/telechargement-climatologie-portail-api-meteofrance/main/tsconfig.json',
-            { directory: import.meta.dirname }
+            'https://raw.githubusercontent.com/AssociationInfoclimat/telechargement-climatologie-meteo-data-gouv/refs/heads/main/README.md',
+            { directory: `${import.meta.dirname}/downloads` }
         );
-        const path = `${import.meta.dirname}/tsconfig.json`;
-        expect(existsSync(path)).toBeTruthy();
+        expect(existsSync(`${import.meta.dirname}/downloads/README.md`)).toBeTruthy();
     });
 });
--- tests/lib/download/downloadUsingWget.integration.spec.ts
+++ tests/lib/download/downloadUsingWget.integration.spec.ts
@@ -1,22 +1,17 @@
 import { downloadUsingWget } from '@/lib/download/downloadUsingWget.js';
 import { existsSync } from 'node:fs';
-import { unlink } from 'node:fs/promises';
+import { rm } from 'node:fs/promises';
 import { beforeEach, describe, expect, it } from 'vitest';
 
 describe('downloadUsingWget', () => {
     beforeEach(async () => {
-        try {
-            await unlink(`${import.meta.dirname}/tsconfig.json`);
-        } catch {
-            //
-        }
+        await rm(`${import.meta.dirname}/downloads`, { recursive: true, force: true });
     });
     it('should download the file to the directory', async () => {
         await downloadUsingWget(
-            'https://raw.githubusercontent.com/AssociationInfoclimat/telechargement-climatologie-portail-api-meteofrance/main/tsconfig.json',
-            { directory: import.meta.dirname }
+            'https://raw.githubusercontent.com/AssociationInfoclimat/telechargement-climatologie-meteo-data-gouv/refs/heads/main/README.md',
+            { directory: `${import.meta.dirname}/downloads` }
         );
-        const path = `${import.meta.dirname}/tsconfig.json`;
-        expect(existsSync(path)).toBeTruthy();
+        expect(existsSync(`${import.meta.dirname}/downloads/README.md`)).toBeTruthy();
     });
 });
