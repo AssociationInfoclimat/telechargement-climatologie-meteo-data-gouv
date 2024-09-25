2649a76833310abeac19bc5f54bad9acc706db8a
jlecordier <jeanlecordier@hotmail.fr>
2024-09-25 11:59:00 +0200

    tests: archives: files: globFrequence: add tests

---
+++ tests/archives/files/globFrequence.spec.ts
@@ -0,0 +1,47 @@
+import { Departement } from '@/archives/departements/Departement.js';
+import { globFrequence } from '@/archives/files/globFrequence.js';
+import { FREQUENCES } from '@/files/Frequence.js';
+import { createInMemoryGlobber } from '@/lib/fs/glob/glob.in-memory.js';
+import { describe, expect, it } from 'vitest';
+
+describe('globFrequence', () => {
+    it('should return files corresponding to the frequency', async () => {
+        const directory = '/my/directory';
+        const globber = createInMemoryGlobber([
+            '/my/directory/MN_01_blabla.gz',
+            '/my/directory/MN_76_blabla.gz',
+            '/my/directory/MN_76_blabla.csv',
+            '/my/directory/H_01_blabla.gz',
+            '/my/directory/H_76_blabla.gz',
+            '/my/directory/H_76_blabla.csv',
+            '/my/directory/Q_01_blabla.gz',
+            '/my/directory/Q_76_blabla.gz',
+            '/my/directory/Q_76_blabla.csv',
+            '/my/directory/MENSQ_01_blabla.gz',
+            '/my/directory/MENSQ_76_blabla.gz',
+            '/my/directory/MENSQ_76_blabla.csv',
+            '/my/directory/DECADQ_01_blabla.gz',
+            '/my/directory/DECADQ_76_blabla.gz',
+            '/my/directory/DECADQ_76_blabla.csv',
+            '/my/directory/DECADAGRO_01_blabla.gz',
+            '/my/directory/DECADAGRO_76_blabla.gz',
+            '/my/directory/DECADAGRO_76_blabla.csv',
+        ]);
+        expect(await globFrequence({ frequence: FREQUENCES.infrahoraire, directory, glob: globber })).toEqual([
+            '/my/directory/MN_01_blabla.gz',
+            '/my/directory/MN_76_blabla.gz',
+        ]);
+        expect(await globFrequence({ frequence: FREQUENCES.decadaireAgro, directory, glob: globber })).toEqual([
+            '/my/directory/DECADAGRO_01_blabla.gz',
+            '/my/directory/DECADAGRO_76_blabla.gz',
+        ]);
+        expect(
+            await globFrequence({
+                frequence: FREQUENCES.horaire,
+                directory,
+                glob: globber,
+                departement: Departement.of(76),
+            })
+        ).toEqual(['/my/directory/H_76_blabla.gz']);
+    });
+});
