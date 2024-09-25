7e55f5ba5f9800762e2c2ff21e6d43243c0d208e
jlecordier <jeanlecordier@hotmail.fr>
2024-09-25 12:05:51 +0200

    fix: lib: fs: glob: in memory: fix pattern

--- src/lib/fs/glob/glob.in-memory.ts
+++ src/lib/fs/glob/glob.in-memory.ts
@@ -2,7 +2,7 @@ import { Globber } from '@/lib/fs/glob/Globber.js';
 
 export function createInMemoryGlobber(files: string[] = []): Globber {
     return function (pattern: string): Promise<string[]> {
-        pattern = pattern.replaceAll('*', '.*');
+        pattern = `${pattern.replaceAll('*', '.*')}$`; // glob pattern to regex pattern
         return Promise.resolve(files.filter(file => file.match(pattern)));
     };
 }
