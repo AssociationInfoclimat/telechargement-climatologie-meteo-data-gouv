fa906815005d4286b8f95b6a5274c4d2043b49e4
jlecordier <jeanlecordier@hotmail.fr>
2024-09-25 17:09:43 +0200

    apps: add saveAllCSVsToDB

---
+++ src/apps/saveAllCSVsToDB.ts
@@ -0,0 +1,89 @@
+import { Departement } from '@/archives/departements/Departement.js';
+import { PrismaDecadairesAgroRepository } from '@/db/decadaires-agro/PrismaRepository.js';
+import { PrismaDecadairesRepository } from '@/db/decadaires/PrismaRepository.js';
+import { PrismaHorairesRepository } from '@/db/horaires/PrismaRepository.js';
+import { PrismaInfrahorairesRepository } from '@/db/infrahoraires/PrismaRepository.js';
+import { PrismaMensuellesRepository } from '@/db/mensuelles/PrismaRepository.js';
+import { PrismaQuotidiennesAutresParametresRepository } from '@/db/quotidiennes/autres-parametres/PrismaRepository.js';
+import { saveCSVsToDB as saveDecadairesAgroCSVsToDB } from '@/decadaires-agro/use-cases/saveCSVsToDB.js';
+import { saveCSVsToDB as saveDecadairesCSVsToDB } from '@/decadaires/use-cases/saveCSVsToDB.js';
+import { saveCSVsToDB as saveHorairesCSVsToDB } from '@/horaires/use-cases/saveCSVsToDB.js';
+import { saveCSVsToDB as saveInfrahorairesCSVsToDB } from '@/infrahoraires/use-cases/saveCSVsToDB.js';
+import { glob } from '@/lib/fs/glob/glob.glob.js';
+import { readLines } from '@/lib/fs/read-lines/readLines.node.js';
+import { LoggerSingleton } from '@/lib/logger/LoggerSingleton.js';
+import { saveCSVsToDB as saveMensuellesCSVsToDB } from '@/mensuelles/use-cases/saveCSVsToDB.js';
+import { saveCSVsToDB as saveQuotidiennesCSVsToDB } from '@/quotidiennes/autres-parametres/use-cases/saveCSVsToDB.js';
+import { PrismaClient } from '@prisma/client';
+
+async function main() {
+    LoggerSingleton.getSingleton().setLogLevel('info');
+
+    LoggerSingleton.getSingleton().info({ message: 'Reading infrahoraires CSVs :' });
+
+    const prisma = new PrismaClient();
+
+    const directory: string = `${process.cwd()}/data`;
+    const departement: Departement | undefined = Departement.of(974);
+
+    await saveInfrahorairesCSVsToDB({
+        directory,
+        globber: glob,
+        lineReader: readLines,
+        repository: new PrismaInfrahorairesRepository({ prisma }),
+        departement,
+    });
+
+    LoggerSingleton.getSingleton().info({ message: 'Reading horaires CSVs :' });
+    await saveHorairesCSVsToDB({
+        directory,
+        globber: glob,
+        lineReader: readLines,
+        repository: new PrismaHorairesRepository({ prisma }),
+        departement,
+    });
+
+    LoggerSingleton.getSingleton().info({ message: 'Reading quotidiennes CSVs :' });
+    await saveQuotidiennesCSVsToDB({
+        directory,
+        globber: glob,
+        lineReader: readLines,
+        repository: new PrismaQuotidiennesAutresParametresRepository({ prisma }),
+        departement,
+    });
+
+    LoggerSingleton.getSingleton().info({ message: 'Reading mensuelles CSVs :' });
+    await saveMensuellesCSVsToDB({
+        directory,
+        globber: glob,
+        lineReader: readLines,
+        repository: new PrismaMensuellesRepository({ prisma }),
+        departement,
+    });
+
+    LoggerSingleton.getSingleton().info({ message: 'Reading decadaires CSVs :' });
+    await saveDecadairesCSVsToDB({
+        directory,
+        globber: glob,
+        lineReader: readLines,
+        repository: new PrismaDecadairesRepository({ prisma }),
+        departement,
+    });
+
+    LoggerSingleton.getSingleton().info({ message: 'Reading decadaires agro CSVs :' });
+    await saveDecadairesAgroCSVsToDB({
+        directory,
+        globber: glob,
+        lineReader: readLines,
+        repository: new PrismaDecadairesAgroRepository({ prisma }),
+        departement,
+    });
+
+    LoggerSingleton.getSingleton().info({ message: 'Done' });
+}
+
+try {
+    await main();
+} catch (e) {
+    LoggerSingleton.getSingleton().error({ data: e });
+}
