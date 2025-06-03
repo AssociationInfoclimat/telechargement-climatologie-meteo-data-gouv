606e9dd34f308ef8c4c61ac50469abfcaa9121f9
jlecordier <jeanlecordier@hotmail.fr>
2025-06-03 16:29:33 +0200

    fix: scripts: update-*: make sure sequential

--- scripts/update-daily.sh
+++ scripts/update-daily.sh
@@ -1,5 +1,5 @@
-cd /opt/telechargement-climatologie-meteo-data-gouv;
-pnpm run update-latest-archives;
-pnpm run download-unzip-update-delete-latest-infrahoraires-csvs:preprod;
-pnpm run download-unzip-update-delete-latest-horaires-csvs:preprod;
-pnpm run download-unzip-update-delete-latest-quotidiennes-csvs:preprod;
+cd /opt/telechargement-climatologie-meteo-data-gouv \
+&& pnpm run update-latest-archives \
+&& pnpm run download-unzip-update-delete-latest-infrahoraires-csvs:preprod \
+&& pnpm run download-unzip-update-delete-latest-horaires-csvs:preprod \
+&& pnpm run download-unzip-update-delete-latest-quotidiennes-csvs:preprod
--- scripts/update-monthly.sh
+++ scripts/update-monthly.sh
@@ -1,2 +1,2 @@
-cd /opt/telechargement-climatologie-meteo-data-gouv;
-pnpm run download-unzip-update-delete-latest-mensuelles-csvs:preprod;
+cd /opt/telechargement-climatologie-meteo-data-gouv \
+&& pnpm run download-unzip-update-delete-latest-mensuelles-csvs:preprod
